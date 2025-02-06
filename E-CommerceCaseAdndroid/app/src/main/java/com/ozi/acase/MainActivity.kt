0fb76f0f55fd0a53e62d48fc8b08eee9284509b2
oguzhanoozer <oguzoozer@gmail.com>
2025-02-06 14:45:50 +0300

    Initial Repo
    Initial projects files added.

---
+++ E-CommerceCaseAdndroid/app/src/main/java/com/ozi/acase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ozi.acase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
